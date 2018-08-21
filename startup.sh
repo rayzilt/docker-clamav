cbddd33200e20f1a338cb942766cc2db8caec911
Silvan Raijer <github@rayzilt.nl>
2018-08-21 20:39:59 +0200

    Cleanup

--- startup.sh
+++ startup.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-echo "Running freshclam to get latest definitions"
+echo "Running freshclam to get latest definitions..."
 freshclam --user=clamav --stdout
 status=$?
+
 if [ ${status} -ne 0 ]; then
 	echo "Couldn't download definitions... stopping."
 	exit ${status}
 fi
 
-echo "Starting freshclam as daemon"
+echo "Starting freshclam as daemon..."
 /usr/bin/freshclam --daemon --stdout &
 status=$?
+
 if [ ${status} -ne 0 ]; then
 	echo "Failed to start freshclam... stopping."
-        exit ${status}
+	exit ${status}
 fi
 
-echo "Starting clamav as daemon"
+echo "Starting clamav as daemon..."
 
 mkdir -p /var/run/clamav
 chown clamav:clamav /var/run/clamav
 
-exec /usr/sbin/clamd 
-
+exec /usr/sbin/clamd
